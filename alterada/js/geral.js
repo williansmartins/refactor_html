5df559db423d8a6eb313661f8152477f0cf24ee1
Willians Martins de Morais <professorwillians@gmail.com>
2015-08-01 16:39:25 -0300

    refactor de js no arquivo alta-performance-desportiva.html do phone

--- alterada/js/geral.js
+++ alterada/js/geral.js
@@ -1,5 +1,5 @@
 //chamada para as fontes
-document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/josefin-slab:n4,n3:all;hammersmith-one:n4:all;open-sans:n8,n6:all;black-ops-one:n4:all;lato:n7,n4,n3,i4:all;league-gothic:n4:all.js" type="text/javascript">\x3C/script>');
+/*document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/josefin-slab:n4,n3:all;hammersmith-one:n4:all;open-sans:n8,n6:all;black-ops-one:n4:all;lato:n7,n4,n3,i4:all;league-gothic:n4:all.js" type="text/javascript">\x3C/script>');
 document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/league-gothic:n4:all;josefin-slab:n3:all;lato:n9,n3,n4,n7,i3:all;hammersmith-one:n4:all;open-sans:n8,n6:all;yellowtail:n4:all.js" type="text/javascript">\x3C/script>');
 document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/josefin-slab:n4,n3:all;hammersmith-one:n4:all;open-sans:n8,n6:all;black-ops-one:n4:all;lato:n7,n4,n3,i4:all;league-gothic:n4:all.js" type="text/javascript">\x3C/script>');
 document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/league-gothic:n4:all;josefin-slab:n3:all;lato:n9,n3,n4,n7,i3:all;hammersmith-one:n4:all;open-sans:n8,n6:all;yellowtail:n4:all.js" type="text/javascript">\x3C/script>');
@@ -8,6 +8,7 @@ document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'h
 document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/josefin-slab:n3:all;amatic-sc:n7:all;lato:n9,i9,n4,n7,i4,n3:all;hammersmith-one:n4:all;league-gothic:n4:all.js" type="text/javascript">\x3C/script>');
 document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/open-sans:n8,n6:all;lato:n4,n7,n9,n3:all;paytone-one:n4:all;league-gothic:n4:all;hammersmith-one:n4:all;black-ops-one:n4:all;josefin-slab:n3:all.js" type="text/javascript">\x3C/script>');
 document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/lato:n9,n3,n4,n7,i4:all;open-sans:n8,n6:all;dosis:n5:all;league-gothic:n4:all;source-sans-pro:n3,n4:all;hammersmith-one:n4:all;josefin-slab:n3:all;yellowtail:n4:all.js" type="text/javascript">\x3C/script>');
+document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/lato:n4,n7,n3,i4:all;josefin-slab:n4,n3:all;cardo:n4:all;league-gothic:n4:all;hammersmith-one:n4:all;black-ops-one:n4:all;open-sans:n8,n6:all.js" type="text/javascript">\x3C/script>');*/
 
 // dependencias do muse com 11 itens
 window.Muse = {};
@@ -23,3 +24,4 @@ window.Muse.assets = {
 // sei la o que é isso
 document.documentElement.className += ' js';
 var __adobewebfontsappname__ = "muse";
+
